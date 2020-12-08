24ba14c5fd153cc4388a233955d922c66171e730
Chris Butner <57114214+chrisbutner@users.noreply.github.com>
2020-12-07 22:53:40 -0800

    Drive debug GUI from Python->JavaScript via websocket; start to visualize policy; still need to plumb real data, set up actual usage flows

--- js/gui.js
+++ js/gui.js
@@ -1,5 +1,79 @@
 ﻿(function () {
-    let config = {
+
+    function highlight(element, proportion) {
+        element.style.backgroundColor = `rgba(128, 0, 0, ${proportion}`;
+    }
+
+    function overlaysHighlightAll() {
+        const proportions = {}
+        for (const square in overlays) {
+            proportions[square] = 0;
+        }
+        for (const move of data.policy) {
+            proportions[move.from] = Math.min(1, proportions[move.from] + move.proportion);
+            proportions[move.to] = Math.min(1, proportions[move.to] + move.proportion);
+        }
+        for (const square in proportions) {
+            highlight(overlays[square], proportions[square]);
+        }
+    }
+
+    function overlaysHighlightMove(move) {
+        for (const overlay of Object.values(overlays)) {
+            overlay.style.backgroundColor = null;
+        }
+        highlight(overlays[move.from], move.proportion);
+        highlight(overlays[move.to], move.proportion);
+    }
+
+    function clearMoves() {
+        document.getElementById("moves").innerHTML = "";
+    }
+
+    function addMove(summary, move) {
+        move.proportion = (move.visit_count / summary.visit_count);
+
+        const element = document.createElement("div");
+        element.className = "move";
+        document.getElementById("moves").appendChild(element);
+
+        const san = document.createElement("span");
+        san.className = "san";
+        san.textContent = move.san;
+        element.appendChild(san);
+
+        const details = document.createElement("span");
+        details.className = "details";
+        details.textContent = `${move.visit_count} / ${summary.visit_count} (${move.proportion})`;
+        element.appendChild(details);
+
+        element.addEventListener("mouseover", () => {
+            highlight(element, move.proportion);
+            overlaysHighlightMove(move);
+        });
+
+        element.addEventListener("mouseout", () => {
+            element.style.backgroundColor = null;
+            overlaysHighlightAll();
+        });
+    }
+
+    function showData(newData) {
+        data = newData;
+        data.summary = { visit_count: data.policy.reduce((total, move) => total + move.visit_count, 0) };
+
+        board.position(data.position);
+        document.getElementById("position").textContent = data.position;
+
+        clearMoves();
+        for (const move of data.policy) {
+            addMove(data.summary, move);
+        }
+
+        overlaysHighlightAll();
+    }
+
+    const config = {
         pieceTheme: "chessboardjs/img/chesspieces/wikipedia/{piece}.png",
         position: "start",
         draggable: true,
@@ -7,25 +81,53 @@
         sparePieces: true,
     };
 
-    let board = Chessboard("board", config);
+    const board = Chessboard("board", config);
+    const squares = {};
+    const overlays = {};
+    let data = { position: "", value: -1, policy: [], summary: { visit_count: 1 } };
 
-    let sparePieces = document.getElementById("sparePieces");
-    let sparePiecesTop = document.querySelector(".spare-pieces-top-4028b");
-    let sparePiecesBottom = document.querySelector(".spare-pieces-bottom-ae20f");
+    const sparePieces = document.getElementById("sparePieces");
+    const sparePiecesTop = document.querySelector(".spare-pieces-top-4028b");
+    const sparePiecesBottom = document.querySelector(".spare-pieces-bottom-ae20f");
 
     document.getElementById("board").appendChild(sparePieces);
     sparePieces.appendChild(sparePiecesBottom);
     sparePieces.appendChild(sparePiecesTop);
 
-    document.getElementById("togglePieces").addEventListener("click", function () {
+    document.getElementById("togglePieces").addEventListener("click", () => {
         sparePieces.style.display = (getComputedStyle(sparePieces, null).display === "none") ? "block" : "none";
     });
 
-    document.getElementById("startButton").addEventListener("click", function () {
+    document.getElementById("startButton").addEventListener("click", () => {
         board.start(false);
     });
 
-    document.getElementById("clearButton").addEventListener("click", function () {
+    document.getElementById("clearButton").addEventListener("click", () => {
         board.clear(false);
+    });
+
+    for (const file of "abcdefgh") {
+        for (let rank = 1; rank <= 8; rank++) {
+            const square = file + rank;
+            const squareElement = document.querySelector(".square-" + square);
+            const squareOverlay = document.createElement("div");
+            squareOverlay.style.width = getComputedStyle(squareElement, null).width;
+            squareOverlay.style.height = getComputedStyle(squareElement, null).height;
+            squareOverlay.style.position = "absolute";
+            squareOverlay.style.top = "0px";
+            squareOverlay.style.left = "0px";
+            squareElement.appendChild(squareOverlay);
+
+            squares[square] = squareElement;
+            overlays[square] = squareOverlay;
+        }
+    }
+
+    const socket = new WebSocket("ws://localhost:8001/");
+    socket.addEventListener("message", (e) => {
+        if (e.data === "ping") {
+            return;
+        }
+        showData(JSON.parse(e.data));
     });
 })();
--- py/gui.py
+++ py/gui.py
@@ -3,22 +3,66 @@ import socketserver
 import functools
 import threading
 import webbrowser
+import websockets
+import asyncio
+import json
+import logging
 
-port = 8000
 serving = False
+port = 8000
+websocket_port = 8001
+websocket_connected = set()
+websocket_loop = None
+
+class HttpHandler(http.server.SimpleHTTPRequestHandler):
+  def log_message(self, format, *args):
+    pass
 
 def _serve():
-  handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory="../js")
-  with socketserver.TCPServer(("", port), handler) as httpd:
+  handler = functools.partial(HttpHandler, directory="../js")
+  with socketserver.TCPServer(("localhost", port), handler) as httpd:
     httpd.serve_forever()
 
-def ensure_serving():
+def _websocket_serve():
+  global websocket_loop
+  websocket_loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(websocket_loop)
+  start_server = websockets.serve(_websocket_handler, "localhost", websocket_port)
+  websocket_loop.run_until_complete(start_server)
+  websocket_loop.run_forever()
+
+def _ensure_serving():
   global serving
   if not serving:
     serving = True
-    thread = threading.Thread(target=_serve)
-    thread.start()
+    threading.Thread(target=_serve).start()
+    threading.Thread(target=_websocket_serve).start()
+    websockets_logger = logging.getLogger("websockets.server")
+    websockets_logger.setLevel(logging.CRITICAL)
+
+async def _websocket_handler(websocket, path):
+  try:
+    websocket_connected.add(websocket)
+    while True:
+      await asyncio.sleep(30)
+      await websocket.send("ping")
+  finally:
+    websocket_connected.remove(websocket)
+
+async def _send(websocket, message):
+  try:
+    return await websocket.send(message)
+  except:
+    pass
+
+def _send_all(websockets, message):
+  if websockets:
+    asyncio.create_task(asyncio.wait([_send(websocket, message) for websocket in websockets]))
 
 def launch():
-  ensure_serving()
+  _ensure_serving()
   webbrowser.open(f"http://localhost:{port}/gui.html")
+
+def show_data(data):
+  message = json.dumps(data)
+  websocket_loop.call_soon_threadsafe(_send_all, websocket_connected, message)
