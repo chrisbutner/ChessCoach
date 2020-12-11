2e690f7ee485de3e6a44f6c08a0db7d8592cf5a9
Chris Butner <57114214+chrisbutner@users.noreply.github.com>
2020-12-10 18:56:31 -0800

    Make network clear in strength test scripts

--- strengthtest_fast.cmd
+++ strengthtest_fast.cmd
@@ -1,3 +1,3 @@
 pushd "%~dp0"
 call conda activate chesscoach
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/STS.epd" -t 200 -s 445.23 -i -242.85
+cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/STS.epd" -t 200 -s 445.23 -i -242.85 -n student
--- strengthtest_slow.cmd
+++ strengthtest_slow.cmd
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 call conda activate chesscoach
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/STS.epd" -t 200 -s 445.23 -i -242.85
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/ERET.epd" -t 10000
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/Arasan20.epd" -t 10000
+cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/STS.epd" -t 200 -s 445.23 -i -242.85 -n student
+cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/ERET.epd" -t 10000 -n student
+cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/Arasan20.epd" -t 10000 -n student
