39933632a44664364c1ecbcaf3eb64c7099eab0c
Chris Butner <57114214+chrisbutner@users.noreply.github.com>
2021-03-27 22:59:27 -0700

    Start another fresh end-to-end run with stochastic weight averaging (SWA) and other fixes/improvements

--- py/alpha.py
+++ py/alpha.py
@@ -32,25 +32,25 @@ tpu_configs = {
 }
 
 deployment_configs = {
-  "selfplay5": {
+  "selfplay6": {
     "roles": {
       "train": {
         "count": 1,
-        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-train:selfplay5_v3",
+        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-train:selfplay6_v10",
         "on_error": "dmesg",
       },
       "play": {
         "count": 19,
-        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-play:selfplay5_v3",
+        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-play:selfplay6_v10",
         "on_error": "dmesg",
       },
     },
   },
-  "selfplay5_pods": {
+  "selfplay6_pods": {
     "roles": {
       "play": {
         "count": 8,
-        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-play:selfplay5_v3",
+        "command": "docker run --rm --privileged --network host --mount type=bind,source=/usr/share/tpu,target=/usr/share/tpu --mount type=bind,source=/lib/libtpu.so,target=/lib/libtpu.so eu.gcr.io/chesscoach/chesscoach-play:selfplay6_v10",
         "on_error": "dmesg",
       },
     },
