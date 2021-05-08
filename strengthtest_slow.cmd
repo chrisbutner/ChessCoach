1e4767c52c1f4c49c9b594903954c797cbd7c930
Chris Butner <57114214+chrisbutner@users.noreply.github.com>
2021-05-08 13:38:05 -0700

    Add strengthtest_sts.sh

--- strengthtest_slow.cmd
+++
@@ -1,5 +0,0 @@
-pushd "%~dp0"
-call conda activate chesscoach
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/STS.epd" -t 200 -s 445.23 -i -242.85 -n student
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/ERET.epd" -t 30000 -n student
-cpp\x64\Release\ChessCoachStrengthTest.exe -e "cpp/StrengthTests/Arasan20.epd" -t 30000 -n student
---
+++ strengthtest_sts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eux
+pushd "$(dirname "$0")"
+
+INSTALLED_DATA="/usr/local/share/ChessCoach"
+
+ChessCoachStrengthTest -e "${INSTALLED_DATA}/STS.epd" -t 200 -s 445.23 -i -242.85 -n student
+
+popd
