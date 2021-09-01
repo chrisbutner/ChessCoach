7beeba9db8b8b82f8ff72a75165526fb6093c4b4
Chris Butner <57114214+chrisbutner@users.noreply.github.com>
2021-08-31 18:10:44 -0700

    Add a script to set up Syzygy 6-5-4-3 in a RAM disk on Cloud TPU VMs (low disk space, but enough memory after node shrink)

---
+++ scripts/ramdisk_syzygy6.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eux
+pushd "$(dirname "$0")"
+
+# It's generally a bad idea to store Syzygy tablebases in a RAM disk, because RAM usage could double
+# as the files are memory-mapped. However, machines such as Cloud TPU VMs have lots of RAM and
+# very little disk by default, so this may be the only option.
+
+CHESSCOACH_DATA="${XDG_DATA_HOME-$HOME/.local/share}/ChessCoach"
+CHESSCOACH_SYZYGY="${CHESSCOACH_DATA}/Syzygy6"
+
+mkdir -p "${CHESSCOACH_SYZYGY}"
+sudo mount -t tmpfs -o size=155g tmpfs "${CHESSCOACH_SYZYGY}"
+
+echo "Syzygy configuration in installed config.toml:"
+grep "syzygy" /usr/local/share/ChessCoach/config.toml
+
+popd
